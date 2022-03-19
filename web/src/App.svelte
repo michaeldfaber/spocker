9ed9273797c4565181b2dc19c611953d02af677c
Michael Faber <michaelf1444@gmail.com>
2022-03-19 13:25:55 -0500

    scaffolded frontend for testing

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,10 +1,29 @@
 <script>
-	export let name;
+	let httpVerb;
+	let endpoint;
+	let expectedJsonResponse;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Spocker</h1>
+
+	<label for="httpVerbInput">HTTP Verb</label>
+	<select id="httpVerbInput">
+		<option value="get">GET</option>
+		<option value="post">POST</option>
+		<option value="put">PUT</option>
+		<option value="update">UPDATE</option>
+		<option value="delete">DELETE</option>
+	</select>
+
+	<label for="endpointInput">Endpoint</label>
+	<input id="endpointInput" type="text" />
+
+	<label for="expectedJsonResponseInput">Expected JSON Response</label>
+	<textarea id="expectedJsonResponseInput" type="text" />
+
+	<br />
+	<button>Create</button>
 </main>
 
 <style>
@@ -16,10 +35,25 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #0099f6;
 		text-transform: uppercase;
 		font-size: 4em;
-		font-weight: 100;
+		font-weight: 200;
+	}
+
+	label {
+		margin-bottom: 5px;
+	}
+
+	select, input {
+		width: 400px;
+		margin-bottom: 30px;
+	}
+
+	textarea {
+		height: 200px;
+		width: 400px;
+		margin-bottom: 30px;
 	}
 
 	@media (min-width: 640px) {
