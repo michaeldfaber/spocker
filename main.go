788a28b9d4633688b3276a2133f8135f75a16bd6
Michael Faber <michaelf1444@gmail.com>
2022-03-21 18:37:46 -0500

    create and delete reading request, added postman collection

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os/exec"
@@ -14,15 +14,47 @@ func main() {
 }
 
 func create_endpoint(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("./scripts/create-endpoint.sh", "GET", "create_test", "{}")
-	_, err := cmd.Output()
-	fmt.Printf("%v", err) // TODO handle err
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+
+	var createEndpointRequest CreateEndpointRequest
+	json.Unmarshal(body, &createEndpointRequest)
+
+	expectedJsonResponse, err := json.Marshal(createEndpointRequest.ExpectedJsonResponse)
+	if err != nil {
+		return
+	}
+
+	cmd := exec.Command(
+		"./scripts/create-endpoint.sh",
+		createEndpointRequest.HttpVerb,
+		createEndpointRequest.Endpoint,
+		string(expectedJsonResponse))
+	_, err = cmd.Output()
+	if err != nil {
+		return
+	}
 }
 
 func delete_endpoint(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("./scripts/delete-endpoint.sh", "GET", "create_test")
-	_, err := cmd.Output()
-	fmt.Printf("%v", err) // TODO handle err
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+
+	var deleteEndpointRequest DeleteEndpointRequest
+	json.Unmarshal(body, &deleteEndpointRequest)
+
+	cmd := exec.Command(
+		"./scripts/delete-endpoint.sh",
+		deleteEndpointRequest.HttpVerb,
+		deleteEndpointRequest.Endpoint)
+	_, err = cmd.Output()
+	if err != nil {
+		return
+	}
 }
 
 func handleRequests() {
@@ -30,21 +62,5 @@ func handleRequests() {
 	router.HandleFunc("/create", create_endpoint).Methods("POST")
 	router.HandleFunc("/delete", delete_endpoint).Methods("POST")
 
-	router.HandleFunc("/hello", hello).Methods("POST")
-	router.HandleFunc("/test", test).Methods("GET")
 	log.Fatal(http.ListenAndServe(":5001", router))
-}
-func test(w http.ResponseWriter, r *http.Request) { // GET
-	w.Header().Set("Content-Type", "application/json")
-	var res interface{}
-	resJson := `{}`
-	json.Unmarshal([]byte(resJson), &res)
-	json.NewEncoder(w).Encode(res)
-}
-func hello(w http.ResponseWriter, r *http.Request) { // POST
-	w.Header().Set("Content-Type", "application/json")
-	var res interface{}
-	resJson := `{}`
-	json.Unmarshal([]byte(resJson), &res)
-	json.NewEncoder(w).Encode(res)
 }
