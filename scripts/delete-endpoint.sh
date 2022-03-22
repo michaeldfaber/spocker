81b58b01c3d6c4191b7c3d5bb8d30d46b3bf17f8
Michael Faber <michaelf1444@gmail.com>
2022-03-22 11:52:31 -0500

    delete bash script updating json (will use db eventually)

--- scripts/delete-endpoint.sh
+++ scripts/delete-endpoint.sh
@@ -11,6 +11,7 @@ fi
 # sh delete-endpoint.sh [http verb] [endpoint name]
 # sh delete-endpoint.sh "POST" "test"
 
+# delete router line from handleRequests()
 function delete_router_handle_func() {
     router_handle=$(grep -n "router.HandleFunc(\"/$2\", $2).Methods(\"$1\")" main.go)
     if [ -z "$router_handle" ]
@@ -22,6 +23,7 @@ function delete_router_handle_func() {
     rm main.go && mv main-temp.go main.go
 }
 
+# delete function
 function delete_handler() {
     handler=$(grep -n "func $2(w http.ResponseWriter, r \*http.Request) { // $1" main.go)
     if [ -z "$handler" ]
@@ -36,5 +38,20 @@ function delete_handler() {
     rm main.go && mv main-temp.go main.go
 }
 
+# update json
+function update_json() {
+    endpoint=$(grep -n "{ \"httpVerb\": \"$1\", \"name\": \"$2\", \"path\": \"$2\"," endpoints.json)
+    if [ -z "$endpoint" ]
+    then
+        return
+    fi
+
+    endpoint_line=$(echo $endpoint | cut -d':' -f 1)
+
+    sed "$endpoint_line"'d' endpoints.json >> endpoints-temp.json
+    rm endpoints.json && mv endpoints-temp.json endpoints.json
+}
+
 delete_router_handle_func $1 $2 &&
-delete_handler $1 $2
+delete_handler $1 $2 &&
+update_json $1 $2
