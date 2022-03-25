e19ca6d4146c79fa80eef316de50bae7b99702d9
Michael Faber <michaelf1444@gmail.com>
2022-03-25 18:13:42 -0500

    validation for create form

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -2,44 +2,66 @@
 	let httpVerb = "GET"
 	let endpoint;
 	let expectedJsonResponse;
-	let createMessage = "";
+	let createMessageSuccess = "";
+	let createMessageFailure = "";
 
-	function handleHttpVerb(e) {
-		httpVerb = e.target.value;
+	function handleHttpVerb(e) { httpVerb = e.target.value; }
+	function handleEndpoint(e) { endpoint = e.target.value; }
+	function handleExpectedJsonResponse(e) { expectedJsonResponse = e.target.value; }
+
+	function updateCreateSuccessMessage(newMessage) {
+		createMessageSuccess = newMessage;
+		createMessageFailure = "";
 	}
 
-	function handleEndpoint(e) {
-		endpoint = e.target.value;
+	function updateCreateFailureMessage(newMessage) {
+		createMessageSuccess = "";
+		createMessageFailure = newMessage;
 	}
 
-	function handleExpectedJsonResponse(e) {
-		expectedJsonResponse = e.target.value;
+	function createFormIsValid() {
+		let validations = "";
+		if (httpVerb === "" || httpVerb === undefined) validations += "HTTP Verb Is Empty. ";
+		if (endpoint === "" || endpoint === undefined) validations += "Endpoint Is Empty. ";
+		if (expectedJsonResponse === "" || expectedJsonResponse === undefined) validations += "Expected JSON Response Is Empty. ";
+
+		if (validations !== "") {
+			updateCreateFailureMessage(validations);
+			return false;
+		}
+
+		return true;
 	}
 
 	async function handleCreate() {
-		const expectedJsonResponseParsed = JSON.parse(expectedJsonResponse)
-		const res = await fetch('http://localhost:5005/create', {
+		if (!createFormIsValid()) return;
+
+		await fetch('http://localhost:5005/create', {
 			method: 'POST',
 			mode: 'cors',
 			body: JSON.stringify({
 				httpVerb: httpVerb,
 				endpoint: endpoint,
-				expectedJsonResponse: expectedJsonResponseParsed
+				expectedJsonResponse: JSON.parse(expectedJsonResponse)
 			})
+		}).catch(_ => {
+			updateCreateFailureMessage("Create Failed");
 		}).then(response => {
-			console.log(response);
 			if (response.ok) {
-				createMessage = "Successfully created " + httpVerb + " " + endpoint + " endpoint!";
+				updateCreateSuccessMessage("Successfully Created " + httpVerb + " " + endpoint + " Endpoint!");
 			} else {
-				createMessage = "Create failed."
+				updateCreateFailureMessage("Create Failed");
 			}
 		})
 	}
+
+
 </script>
 
 <main>
 	<h1>Spocker</h1>
 
+	<!-- Create Form -->
 	<label for="httpVerbInput">HTTP Verb</label>
 	<select id="httpVerbInput" on:change={handleHttpVerb}>
 		<option value="GET">GET</option>
@@ -58,8 +80,12 @@
 	<br />
 	<button id="create" on:click={() => handleCreate()}>Create</button>
 
-	<br />
-	<div id="create-message">{createMessage}</div>
+	<div id="create-message-success">{createMessageSuccess}</div>
+	<div id="create-message-failure">{createMessageFailure}</div>
+
+	<!-- Dashboard -->
+	<!-- TODO -->
+
 </main>
 
 <style>
@@ -70,6 +96,7 @@
 	}
 
 	h1 {
+		margin-top: 20px;
 		color: #0099f6;
 		text-transform: uppercase;
 		font-size: 4em;
@@ -97,6 +124,16 @@
 		font-weight: 500;
 		height: 40px;
 		width: 150px;
+	}
+
+	#create-message-success {
+		margin-top: 20px;
+		color: #0099f6;
+	}
+
+	#create-message-failure {
+		margin-top: 20px;
+		color: red;
 	}
 
 	@media (min-width: 640px) {
